08b29c6555259157d33f25958642b31c0abe6416
ivanstan <ivanstan@gmail.com>
2024-05-25 17:39:27 +0200

    Vite project

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ function App() {
   const [popustPlacanje, setPopustPlacanje] = useState(true);
   const [taksaMedijskiServis, setTaksaMedijskiServis] = useState(true);
 
+  const [obracunskaSnaga, setObracunskaSnaga] = useState(11.04);
+  const [proizvedenaElEnergija, setProizvedenaElEnergija] = useState(1000);
+  const [brojDana, setBrojDana] = useState(31); // ToDo: mozda u odnosu na mesec
+
   const [prethodnoPreuzetoVT, setPrethodnoPreuzetoVT] = useState(10554);
   const [prethodnoPreuzetoNT, setPrethodnoPreuzetoNT] = useState(9492);
   const [prethodnoIsporucenoVT, setPrethodnoIsporucenoVT] = useState(10522);
@@ -143,20 +147,45 @@ function App() {
     <table border="1">
       <tbody>
       <tr>
-        <th colSpan="2">Obračunska snaga:</th>
-        <td colSpan="10">11.04 kW</td>
+        <th>Obračunska snaga:</th>
+        <td>
+          <TextField
+            type="number"
+            // inputProps={{ step: "0.01" }}
+            variant="outlined"
+            value={obracunskaSnaga}
+            onChange={(e) => setObracunskaSnaga(e.target.value)}
+          />kW
+        </td>
       </tr>
       <tr>
-        <th colSpan="2">Proizvedena el. energija:</th>
-        <td colSpan="10">1,000 0</td>
+        <th>Proizvedena el. energija:</th>
+        <td>
+          <TextField
+            type="number"
+            // inputProps={{ step: "0.01" }}
+            variant="outlined"
+            value={proizvedenaElEnergija}
+            onChange={(e) => setProizvedenaElEnergija(e.target.value)}
+          />
+          0
+        </td>
       </tr>
       <tr>
-        <th colSpan="2">Isporučena el. energija:</th>
-        <td colSpan="10">0 kW/h</td>
+        <th>Isporučena el. energija:</th>
+        <td>0 kW/h</td>
       </tr>
       <tr>
-        <th colSpan="2">Broj dana:</th>
-        <td colSpan="10">31</td>
+        <th>Broj dana:</th>
+        <td>
+          <TextField
+            type="number"
+            // inputProps={{ step: "0.01" }}
+            variant="outlined"
+            value={brojDana}
+            onChange={(e) => setBrojDana(e.target.value)}
+          />
+        </td>
       </tr>
       </tbody>
     </table>
@@ -305,6 +334,210 @@ function App() {
       </tbody>
     </table>
 
+    <table border="1">
+      <tr>
+        <th colSpan="4">OBRAČUN ZA ELEKTRIČNU ENERGIJU</th>
+      </tr>
+      <tr>
+        <th></th>
+        <th rowSpan="2">TARIFA</th>
+        <th>Utrošeno (kW/kWh)</th>
+        <th>Cena po jedinici</th>
+        <th>Iznos (dinara)</th>
+      </tr>
+      <tr>
+        <th colSpan="5"></th>
+      </tr>
+      <tr>
+        <th colSpan="5">Troškovi koje nezavisne od potrošnje električne energije</th>
+      </tr>
+      <tr>
+        <td>1.</td>
+        <td>Obračunska snaga (kW)</td>
+        <td>11.04</td>
+        <td>54.258</td>
+        <td>599.01</td>
+      </tr>
+      <tr>
+        <td>2.</td>
+        <td>Trošak garantovanog snabdevača</td>
+        <td colSpan="3">160.67</td>
+      </tr>
+      <tr>
+        <th></th>
+        <th>Utrošena električna energija</th>
+        <td colSpan="3"></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Zelena zona - Viša tarifa (VT)</td>
+        <td>423</td>
+        <td>9.1092</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Zelena zona - Niža tarifa (NT)</td>
+        <td>362</td>
+        <td>2.2773</td>
+        <td>824.38</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Plava zona - Viša tarifa (VT)</td>
+        <td></td>
+        <td>13.6638</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Plava zona - Niža tarifa (NT)</td>
+        <td>61</td>
+        <td>3.4160</td>
+        <td>208.38</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Crvena zona - Viša tarifa (VT)</td>
+        <td></td>
+        <td>27.3276</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Crvena zona - Niža tarifa (NT)</td>
+        <td></td>
+        <td>6.8319</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>3.</td>
+        <th>UKUPNO ZA UTROŠENU ELEKTRIČNU ENERGIJU U OBRAČUNSKOM PERIODU</th>
+        <td colSpan="3">1,032.76</td>
+      </tr>
+      <tr>
+        <td></td>
+        <th>Preuzeta električna energija</th>
+        <td colSpan="3"></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Zelena zona - Viša tarifa (VT)</td>
+        <td>255</td>
+        <td>9.1092</td>
+        <td>2,322.85</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Niža tarifa (NT)</td>
+        <td>107</td>
+        <td>2.2773</td>
+        <td>243.67</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Plava zona - Viša tarifa (VT)</td>
+        <td>761</td>
+        <td>13.6638</td>
+        <td>10,398.15</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Niža tarifa (NT)</td>
+        <td>319</td>
+        <td>3.4160</td>
+        <td>1,089.7</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Crvena zona - Viša tarifa (VT)</td>
+        <td></td>
+        <td>27.3276</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Niža tarifa (NT)</td>
+        <td></td>
+        <td>6.8319</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>4.</td>
+        <th>UKUPNO ZA PREUZETU ELEKTRIČNU ENERGIJU U OBRAČUNSKOM PERIODU</th>
+        <td colSpan="3">14,054.37</td>
+      </tr>
+      <tr>
+        <td>5.</td>
+        <td>Popust 5% za plaćanje prethodnog računa u roku dospeća</td>
+        <td colSpan="3">-50.00</td>
+      </tr>
+      <tr>
+        <td>6.</td>
+        <td>Popust za elektronsku dostavu računa</td>
+        <td colSpan="3">-50.00</td>
+      </tr>
+      <tr>
+        <td>7.</td>
+        <td>Naknada za podsticaj povlašćenih proizvođača el. en.</td>
+        <td>1,442</td>
+        <td>0.801</td>
+        <td>1,155.04</td>
+      </tr>
+      <tr>
+        <td>8.</td>
+        <td>Naknada za unapređenje energetske efikasnosti</td>
+        <td>423</td>
+        <td>0.015</td>
+        <td>6.35</td>
+      </tr>
+      <tr>
+        <td>9.</td>
+        <td>Naknada za obr. prist. DS za razliku preuzete i utrošene el. en.</td>
+        <td>3.879</td>
+        <td>0.970</td>
+        <td>3,943.97</td>
+      </tr>
+      <tr>
+        <td>10.</td>
+        <td>Osnovica za obračun akcize (1+2+3+5+6+7+8+9)</td>
+        <td colSpan="3">6,847.80</td>
+      </tr>
+      <tr>
+        <td>11.</td>
+        <td>Iznos akcize (stopa 7,5%)</td>
+        <td colSpan="3">513.59</td>
+      </tr>
+      <tr>
+        <td>12.</td>
+        <td>Osnovica za PDV (9+10)</td>
+        <td colSpan="3">7,361.39</td>
+      </tr>
+      <tr>
+        <td>13.</td>
+        <td>Iznos PDV (20%)</td>
+        <td colSpan="3">1,472.28</td>
+      </tr>
+      <tr>
+        <td>14.</td>
+        <td>Umanjenje za energetski ugrožene kupce</td>
+        <td colSpan="3">0.00</td>
+      </tr>
+      <tr>
+        <td>15.</td>
+        <td>ZADUŽENJE ZA OBRAČUNSKI PERIOD (1+2+3+5+6+7+8+9+11+13+14)</td>
+        <td colSpan="3">8,833.66</td>
+      </tr>
+      <tr>
+        <td>16.</td>
+        <td>Taksa za javni medijski servis (ne ulazi u osnovicu za PDV po čl. 17,st.4,t.2ЗPDV)</td>
+        <td colSpan="3">299.00</td>
+      </tr>
+      <tr>
+        <th colSpan="2">UKUPNO ZADUŽENJE ZA OBRAČUNSKI PERIOD (15+16)</th>
+        <td colSpan="3">9,132.66</td>
+      </tr>
+    </table>
 
     <table border="1">
       <tr>
@@ -331,7 +564,7 @@ function App() {
         <td>kWh</td>
       </tr>
       <tr>
-        <th>DIRETNO POTROŠENO</th>
+        <th>DIREKTNO POTROŠENO</th>
         <td>669</td>
         <td>kWh</td>
       </tr>
